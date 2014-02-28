3d210adfb8730442d0ebd875f0c5c29d4dc2e5fc
kojoman <juantuah@gmail.com>
2014-02-27 23:57:08 -0500

    Post Data using Ajax

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,12 +4,37 @@
 $(document).ready(function() {
 
     // process form data
-    var formData = {
+    $('form').submit(function(event) {
 
-    };
+        // get the form data
+        var formData = {
+            'to'            : $('input[name=to]').val(),
+            'from'          : $('input[name=from]').val(),
+            'message'       : $('input[name=message]').val(),
+            'date'          : $('input[name=date]').val(),
+            'hour'          : $('input[name=hour]').val(),
+            'minute'          : $('input[name=minute]').val()
+        };
 
-    // process the form
-    $.ajax({
-        
-    })
-})
+        // process the form
+        $.ajax({
+            type        : 'POST',
+            url         : 'sms.php',
+            'data'      : formData,
+            dataType    : 'json'
+        })
+
+        // Use the done promise callback
+            .done(function(data) {
+                // log data to console - just for test purposes
+                console.log("Dumping data");
+                console.log(data);
+
+                // handle errors
+            });
+
+        // stop form from submitting the normal way and refresh page
+        event.preventDefault();
+    });
+
+});
