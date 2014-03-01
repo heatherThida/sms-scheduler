95cc97d7934d99ed44daabdb696bed571d10e6cb
kojoman <juantuah@gmail.com>
2014-02-28 23:55:41 -0500

    Use a loop to get date and time values to simplify the process.

--- includes/common.php
+++ includes/common.php
@@ -12,3 +12,11 @@ function debug($object){
     echo "</pre>";
 }
 
+function selectOptions($begin, $end){
+
+    for($i = $begin; $i < $end; $i++) {
+        echo '<option value="' . $i . '" >' . $i . '</option>';
+    }
+//    <option value="00" selected="selected">00</option>
+//                            <option value="01" >01</option>
+}
