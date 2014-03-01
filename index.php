a343a786bac199753b40c6a11308aa03acf77509
kojoman <juantuah@gmail.com>
2014-02-28 23:57:12 -0500

    Put form elements in div so that we can use javascript on them.

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+    include_once('./includes/common.php');
+?>
+
+<?php
     $ip = getenv('HTTP_CLIENT_IP')?:
           getenv('HTTP_X_FORWARDED_FOR')?:
           getenv('HTTP_X_FORWARDED')?:
@@ -18,104 +22,44 @@
 	</head>
 	<body>
 		<div class="container">
-			<form class="form-schedule-sms" role="form" action="sms.php" method="post">
-				<fieldset>
-					To: <input type="text" name="to" placeholder="(212) 000-0000"><br />
-					From: <input type="text" name="from" placeholder="(212) 111-1111"><br />
-					Message: <textarea maxlength="140" name="message"></textarea><br />
-					Date: <input type="text" id="date" name="date"><br />
-                    Hour: <select id="hour" name="hour">
-                            <option value="00" selected="selected">00</option>
-                            <option value="01" >01</option>
-                            <option value="02" >02</option>
-                            <option value="03" >03</option>
-                            <option value="04" >04</option>
-                            <option value="05" >05</option>
-                            <option value="06" >06</option>
-                            <option value="07" >07</option>
-                            <option value="08" >08</option>
-                            <option value="09" >09</option>
-                            <option value="10" >10</option>
-                            <option value="11" >11</option>
-                            <option value="12" >12</option>
-                            <option value="13" >13</option>
-                            <option value="14" >14</option>
-                            <option value="15" >15</option>
-                            <option value="16" >16</option>
-                            <option value="17" >17</option>
-                            <option value="18" >18</option>
-                            <option value="19" >19</option>
-                            <option value="20" >20</option>
-                            <option value="21" >21</option>
-                            <option value="22" >22</option>
-                            <option value="23" >23</option>
+            <div class="col-sm-6 col-sm-offset-3">
+
+                <form name="sms-form" id="sms-form" class="form-schedule-sms" role="form" action="sms.php" method="post">
+                    <fieldset>
+                        <!-- SMS TO -->
+                        <div id=""to-group" class="form-group">
+                            <label for="to">To</label>
+                            <input type="text" class="form-control" name="to" id="to" placeholder="(212) 000-0000">
+                        </div>
+                        <!-- SMS From -->
+                        <div id="from-group" class="form-group">
+                            <label for="from">From</label>
+                            <input type="text" class="form-control" name="from" id="from" placeholder="(212) 000-1111">
+                        </div>
+                        <!-- SMS Message -->
+                        <div id="message-group" class="form-group">
+                            <label for="message">Message</label>
+                            <textarea maxlength="140" name="message" id="message" required></textarea>
+                        </div>
+
+                        <!-- Date to schedule SMS -->
+                        <div id="message-group" class="form-group">
+                            <label for="message">Date</label>
+                            <input type="text" id="date" name="date">
+                        </div>
+
+
+                        Hour: <select id="hour" name="hour">
+                                <?php selectOptions(0, 24); ?>
                         </select>
-                    Minute: <select id="minute" name="minute">
-                            <option value="00" selected="selected">00</option>
-                            <option value="01" >01</option>
-                            <option value="02" >02</option>
-                            <option value="03" >03</option>
-                            <option value="04" >04</option>
-                            <option value="05" >05</option>
-                            <option value="06" >06</option>
-                            <option value="07" >07</option>
-                            <option value="08" >08</option>
-                            <option value="09" >09</option>
-                            <option value="10" >10</option>
-                            <option value="11" >11</option>
-                            <option value="12" >12</option>
-                            <option value="13" >13</option>
-                            <option value="14" >14</option>
-                            <option value="15" >15</option>
-                            <option value="16" >16</option>
-                            <option value="17" >17</option>
-                            <option value="18" >18</option>
-                            <option value="19" >19</option>
-                            <option value="20" >20</option>
-                            <option value="21" >21</option>
-                            <option value="22" >22</option>
-                            <option value="23" >23</option>
-                            <option value="24" >24</option>
-                            <option value="25" >25</option>
-                            <option value="26" >26</option>
-                            <option value="27" >27</option>
-                            <option value="28" >28</option>
-                            <option value="29" >29</option>
-                            <option value="30" >30</option>
-                            <option value="31" >31</option>
-                            <option value="32" >32</option>
-                            <option value="33" >33</option>
-                            <option value="34" >34</option>
-                            <option value="35" >35</option>
-                            <option value="36" >36</option>
-                            <option value="37" >37</option>
-                            <option value="38" >38</option>
-                            <option value="39" >39</option>
-                            <option value="40" >40</option>
-                            <option value="41" >41</option>
-                            <option value="42" >42</option>
-                            <option value="43" >43</option>
-                            <option value="44" >44</option>
-                            <option value="45" >45</option>
-                            <option value="46" >46</option>
-                            <option value="47" >47</option>
-                            <option value="48" >48</option>
-                            <option value="49" >49</option>
-                            <option value="50" >50</option>
-                            <option value="51" >51</option>
-                            <option value="52" >52</option>
-                            <option value="53" >53</option>
-                            <option value="54" >54</option>
-                            <option value="55" >55</option>
-                            <option value="56" >56</option>
-                            <option value="57" >57</option>
-                            <option value="58" >58</option>
-                            <option value="59" >59</option>
+                        Minute: <select id="minute" name="minute">
+                            <?php selectOptions(0, 60); ?>
                         </select>
-                    <input type="hidden" name="ip" value="<?php echo $ip ?>" />
-					<button class="btn btn-lg btn-success btn-block" type="submit">Submit</button>
-				</fieldset>
-			</form>
+                        <input type="hidden" name="ip" value="<?php echo $ip ?>" />
+                        <button class="btn btn-lg btn-success btn-block" type="submit">Submit</button>
+                    </fieldset>
+            </form>
+            </div> <!-- End col-sm-6 -->
 		</div> <!-- /container -->
 	
 		<?php include 'footer.php'; ?>
