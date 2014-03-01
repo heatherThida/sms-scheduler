26754625b7d9a598e48ae804556eb0a23efb2101
kojoman <juantuah@gmail.com>
2014-03-01 01:44:26 -0500

    Scheduler Class to load sms from Database and schedule them to be sent

---
+++ Sms/Scheduler.php
@@ -0,0 +1,20 @@
+<?php
+
+include('../includes/config.php');
+
+/*
+ * Class to schedule SMS
+ */
+class SmsScheduler {
+
+
+
+    private $scheduledTime;
+
+    public function readDatabase() {
+
+    }
+    public function getScheduledTime() {
+
+    }
+}
