f8194cbaaf234197304852d277eae621391bf394
kojoman <juantuah@gmail.com>
2014-02-28 23:57:53 -0500

    Implement datetimepicker and validation in custom js file

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,17 +4,41 @@
 $(document).ready(function() {
 
     // jQuery UI Datepicker
-    $(function() {
-        $("#date").datepicker({
-            //options go here
-            showOtherMonths: true,
-            selectOtherMonths: true,
-            minDate: 0,
-            maxDate: "+3M"
-        });
-        $("#anim").change(function() {
-            $("#date").datepicker("option", "showAnim", $(this).val() )
-        })
+//    $(function() {
+//        $("#date").datepicker({
+//            //options go here
+//            showOtherMonths: true,
+//            selectOtherMonths: true,
+//            minDate: 0,
+//            maxDate: "+3M"
+//        });
+//        $("#anim").change(function() {
+//            $("#date").datepicker("option", "showAnim", $(this).val() )
+//        })
+//    });
+
+    // Date time picker
+    $(function(){
+       $('*[name=date]').appendDtpicker({
+           'inline'             : false,
+           'minuteInterval'     : 5,
+           'futureOnly'         : true,
+           'closeOnSelected'    : true
+       });
+    });
+
+    //Validation
+    $("#sms-form").validate({
+        rules: {
+            to: {
+                required: true,
+                phoneUS: true
+            },
+            from: {
+                required: true,
+                phoneUS: true
+            }
+        }
     });
 
     // process form data
@@ -24,10 +48,11 @@ $(document).ready(function() {
         var formData = {
             'to'            : $('input[name=to]').val(),
             'from'          : $('input[name=from]').val(),
-            'message'       : $('input[name=message]').val(),
+            'message'       : $('textarea[name=message]').val(),
             'date'          : $('input[name=date]').val(),
-            'hour'          : $('input[name=hour]').val(),
-            'minute'          : $('input[name=minute]').val()
+            'hour'          : $('select[name=hour]').val(),
+            'minute'        : $('select[name=minute]').val(),
+            'ip'            : $('input[name=ip]').val()
         };
 
         // process the form
@@ -44,11 +69,15 @@ $(document).ready(function() {
                 console.log("Dumping data");
                 console.log(data);
 
-                // handle errors
+                // handle errors and validation
+                if (! data.success) {
+                    //handle errors for to
+                    $('#to-group').addClass('has-error');
+                }
             });
 
         // stop form from submitting the normal way and refresh page
-        //event.preventDefault();
+        event.preventDefault();
     });
 
 });
