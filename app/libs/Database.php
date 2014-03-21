0547551ccda0f8d3d6894ef24d23e6ce7865b666
kojoman <juantuah@gmail.com>
2014-03-20 23:54:02 -0400

    Database Class.

---
+++ app/libs/Database.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class Database
+ *
+ * Creates a PDO connection that will be passed into models. This helps pass same
+ * database connection for all  models and prevent opening multiple connections at once.
+ */
+
+class Database extends PDO
+{
+    /**
+     * Constructor
+     *
+     * Create a PDO instance representing a connection to a database
+     */
+    public function __construct()
+    {
+        // Set the options parameter for the PDO connection.
+        $options = array(
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        );
+
+        // Generate a database connection using the PDO connector 
+        parent::__construct(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS, $options);
+    }
+} 
