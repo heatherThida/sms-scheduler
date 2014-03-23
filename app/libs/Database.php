1d7108bbb013ea7383aed4bb76e3623b254046f0
kojoman <juantuah@gmail.com>
2014-03-22 22:47:11 -0400

    Minor diff

--- app/libs/Database.php
+++ app/libs/Database.php
@@ -21,7 +21,7 @@ class Database extends PDO
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
         );
 
-        // Generate a database connection using the PDO connector 
+        // Generate a database connection using the PDO connector
         parent::__construct(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS, $options);
     }
 } 
